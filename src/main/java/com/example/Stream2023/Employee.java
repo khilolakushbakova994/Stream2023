a89f718f9a49463a0eca2f9f96a8506dea914c97
khilola.kushbakova <xilola_kushbakova@mai.ru>
2023-11-28 11:31:02 +0200

    Initial commit

---
+++ src/main/java/com/example/Stream2023/Employee.java
@@ -0,0 +1,2 @@
+package com.example.Stream2023;public class Employee {
+}
---
+++ src/main/java/com/example/Stream2023/Stream2023Application.java
@@ -0,0 +1,13 @@
+package com.example.Stream2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stream2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stream2023Application.class, args);
+	}
+
+}
